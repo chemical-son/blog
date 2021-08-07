417f3df0930a9e1b93814917e17c5c2b80ded31f
hex <ongoingprodigy@gmail.com>
2021-08-07 11:03:18 -0400

    uploading the project

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Http\Response;
+
+class AuthController extends Controller
+{
+    //
+    public function __construct()
+    {
+        $this->middleware('guest')->only(['login','register']);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'=>'string|required',
+            'email' => 'required|email|unique:users,email',
+            'phone' => 'required|digits:11'
+        ]);
+        $user = User::create([
+            'name'=> $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone
+        ]);
+        
+        $token = $user->createToken('key')->plainTextToken;
+        return response(['user' => $user, 'token' => $token], 201);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return ['message' => 'logout successfully'];
+    }
+
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+        $user = User::where('email', $request['email'])->first();
+        if(!$user)
+        {
+            return response(['error' => 'invalid email'],401);
+        }
+        
+        $token = $user->createToken('key')->plainTextToken;
+        return response(['user' => $user, 'token' => $token], 201);
+    }
+}
